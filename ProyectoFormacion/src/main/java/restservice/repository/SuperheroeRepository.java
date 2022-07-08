3bdf15655172b0e0536c4dd0f8b0ee74f4a0d174
JoseMarchante <Jose@DESKTOP-CA6QLM6.mshome.net>
2022-07-08 21:32:22 +0200

    Se actualizan los repositorios

--- ProyectoFormacion/src/main/java/restservice/repository/SuperheroeRepository.java
+++ ProyectoFormacion/src/main/java/restservice/repository/SuperheroeRepository.java
@@ -2,16 +2,14 @@ package restservice.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import restservice.model.Superheroe;
 
 
 
 @Repository
-public interface SuperheroeRepository extends CrudRepository<Superheroe, Long>{
-	@Query("select u from superheroe s where s.nombre like %?1%")
-	List<Superheroe> buscarSuperheroePorNombre(String nombre);
+public interface SuperheroeRepository extends JpaRepository<Superheroe, Integer>{
+	List<Superheroe> findByNombreContaining(String nombre);
+	
 }
